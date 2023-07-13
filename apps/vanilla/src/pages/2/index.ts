e7d1aa7ffec1b39ecf1517bc778db04446462b49
EgorRyazanov <aricsybsn@gmail.com>
2023-07-13 10:52:46 +0500

    Resolve linting JC-533

--- apps/vanilla/src/pages/2/index.ts
+++ apps/vanilla/src/pages/2/index.ts
@@ -2,11 +2,11 @@ import { Controller } from './src/controllers';
 import { PlayerContainerView } from './src/views/players-container-view';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const app = document.getElementById('app');
-  const container = new PlayerContainerView();
-  const players = ['John', 'Jack'];
-  if (app) {
-    const controller = new Controller(app, container, players);
-    controller.init();
-  }
+	const app = document.getElementById('app');
+	const container = new PlayerContainerView();
+	const players = ['John', 'Jack'];
+	if (app && players.length > 0) {
+		const controller = new Controller(app, container, players);
+		controller.init();
+	}
 });
