7b0fbe749a925fa9541b47311e73610e96867e62
AricsyGIT <aricsybsn@gmail.com>
2023-08-01 11:45:44 +0500

    Make catch form error pipe JC-549

--- apps/angular/src/core/utils/catch-form-error.ts
+++ apps/angular/src/core/utils/catch-form-error.ts
@@ -1,12 +1,25 @@
 import { FormGroup } from '@angular/forms';
+import { AppError } from '@js-camp/core/models/app-error';
 import { Observable, OperatorFunction, catchError, throwError } from 'rxjs';
 
 export function catchFormErrors<T>(form: FormGroup): OperatorFunction<T, T> {
 	return (source$: Observable<T>) =>
 		source$.pipe(
-			catchError((error) => {
-				console.log(form.controls);
-				return throwError(() => error);
+			catchError((errors) => {
+				if (errors instanceof Array) {
+					const commonErrors: AppError[] = [];
+					errors.forEach((error) => {
+						if (error instanceof AppError) {
+							if (form.contains(error.key)) {
+								form.controls[error.key].setErrors({ invalid: error.message });
+							} else {
+								commonErrors.push(error);
+							}
+						}
+					});
+					errors = commonErrors;
+				}
+				return throwError(() => errors);
 			})
 		);
 }
--- libs/core/mappers/error.mapper.ts
+++ libs/core/mappers/error.mapper.ts
@@ -2,7 +2,18 @@ import { ErrorDto } from '../dtos/error.dto';
 import { AppError } from '../models/app-error';
 
 export namespace ErrorMapper {
+	export const COMMON_ERROR_FIELD = 'common';
+
+	const FORM_FIELD_FROM_DTO: Record<string, string> = {
+		email: 'email',
+		password: 'password',
+		first__name: 'firstName',
+		last__name: 'lastName',
+	};
+
 	export function fromDto(errors: ErrorDto[]): AppError[] {
-		return errors.map((error) => new AppError(error.detail, error.attr, error.code));
+		return errors.map(
+			(error) => new AppError(error.detail, FORM_FIELD_FROM_DTO[error.attr] ?? COMMON_ERROR_FIELD, error.code)
+		);
 	}
 }
