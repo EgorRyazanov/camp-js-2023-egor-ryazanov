1e7087f6714975235bef5ebfbb7016aa36d0df90
AricsyGIT <aricsybsn@gmail.com>
2023-08-10 11:02:10 +0500

    Replace getting url to at signature JC-549

--- apps/angular/src/app/features/anime/animes-page/animes-page.component.ts
+++ apps/angular/src/app/features/anime/animes-page/animes-page.component.ts
@@ -140,7 +140,7 @@ export class AnimesPageComponent {
 	 * @param params Changed params.
 	 */
 	private setQueryParams(params: Partial<AnimeRoutingQueryParams>): void {
-		const urlWithoutParams = this.router.url.split('?')[0];
+		const urlWithoutParams = this.router.url.split('?').at(0);
 		this.router.navigate([urlWithoutParams], { queryParams: { ...this.queryParams, ...params } });
 	}
 
